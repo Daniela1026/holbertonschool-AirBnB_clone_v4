073a86ee411d14ee3bb904c4b3167956062fb334
Caballero018 <4617@holbertonstudents.com>
2023-01-04 17:27:05 -0500

    Add of checkbox in amenities

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,11 @@
-const $ = check.$;
 $(document).ready(function () {
     const amenities = {};
-    $('INPUT[type="checkbox"]').change(function (e) { 
-        e.preventDefault();
-        if ($(this).is(':checked')) {
+    $('input[type="checkbox"]').change(function () {
+        if ($(this).prop('checked')) {
             amenities[$(this).attr('data-id')] = $(this).attr('data-name');
         }
-        else delete amenities[$(this).attr('id')];
-        $('.amenities h4').text(Object.values(amenities).join(', '));
+        else delete amenities[$(this).attr('data-id')];
+        $('.amenities h4').html(Object.values(amenities).join(', '));
     });
 
 });
