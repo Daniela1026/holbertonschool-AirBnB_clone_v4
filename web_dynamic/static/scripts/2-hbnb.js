e2af85e4c6aaf76bf135d1315bb1bf1986287057
Michel <michi@LAPTOP-6PJNB5H3.localdomain>
2023-01-05 00:12:27 -0500

    Check if status od derminated url is OK

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(function () {
     const amenities = {};
     $('input[type="checkbox"]').change(function () {
         if ($(this).prop('checked')) {
@@ -7,12 +7,12 @@ $(document).ready(function () {
         else delete amenities[$(this).attr('data-id')];
         $('.amenities h4').html(Object.values(amenities).join(', '));
     });
-    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    const url = 'http://0.0.0.0:5001/api/v1/status/'
+    $.get(url, function (data) {
     if (data.status === 'OK') {
       $('#api_status').addClass('available');
     } else {
-        $('div#api_status').removeClass('available');
-        $('div#api_status').css({ 'background-color':'#cccccc' })
+      $('#api_status').removeClass('available');
     }
   });
 });
